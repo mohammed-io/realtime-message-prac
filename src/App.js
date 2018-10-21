610e08cca61278688b74dca4f5b1c165af6eb69c
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2018-10-21 18:03:39 +0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAsKbsjPKuH4MCWbfcFgq10UtafKdYzSFo",
+  authDomain: "nyan-4be65.firebaseapp.com",
+  databaseURL: "https://nyan-4be65.firebaseio.com",
+  projectId: "nyan-4be65",
+  storageBucket: "",
+  messagingSenderId: "1080099173418"
+});
+
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+
+Array.prototype.groupBy = function(key) {
+  return this.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+}
+
+
+const FireStore = firebase.firestore();
 
 class App extends Component {
+  state = {
+    messages: [],
+    users: [],
+    username: "mohammed"
+  };
+
+  componentDidMount() {
+
+    FireStore.collection('users')
+      .onSnapshot(snap => {
+        this.setState({users: snap.docs.map(d => d.data())
+          .filter(u => u.username != this.state.username)})
+      })
+
+    FireStore.collection("nyanise")
+      .where("to", "==", this.state.username)
+      .onSnapshot(snap => {
+        this.setState({
+          messages: snap.docs.map(d => ({ ...d.data(), id: d.id }))
+        }, () => console.log(this.state.messages.groupBy('to')));
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const grouped = this.state.messages.groupBy('from');
+
+    return (<div>
+      <p>Users:</p>
+      <ul>
+        {
+          this.state.users.map(u => <li>{u.username}</li>)
+        }
+      </ul>
+
+      {Object.keys(grouped).map(k => (
+        <div>
+          <p>Messages from {k}</p>
+          <ul>
+            {grouped[k].map(x => (<li>{x.text}</li>))}
+          </ul>
+        </div>
+      ))}
+    </div>);
   }
 }
 
